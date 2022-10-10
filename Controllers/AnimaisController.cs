adbe4756413041c521eec22bf70c332c2978c8d5
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-10 13:03:48 -0300

    aula com alunos stone

---
+++ Controllers/AnimaisController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+using WebMVC.Servicos;
+
+namespace WebMVC.Controllers
+{
+    public class AnimaisController : Controller
+    {
+        // [Route("/animais-domesticos")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(string nome)
+        {
+            AnimalServico.Instancia().Add(new Animal() { 
+                Nome = nome
+            });
+
+            ViewBag.mensagem = "Cadastrado com sucesso";
+            return View();
+        }
+    }
+}
